df8be17dde735637774c8373c40ad7221222a6fd
AyanicD <ayan@attentive.ai>
2023-03-22 18:30:27 +0530

    GCS chunk issue

---
+++ chunkedupload/uploads/routers.py
@@ -0,0 +1,19 @@
+from rest_framework import routers
+
+from uploads.views import ChunkedUploadApiViewSet
+
+class BaseRouter(routers.SimpleRouter):
+    """Base Router class to be inherited by all Router classes."""
+
+    def extend(self, extended_router=None):
+        if extended_router:
+            self.registry.extend(extended_router.registry)  # noqa
+
+
+CommonRouter = BaseRouter()
+CommonRouter.register(
+    "api",
+    ChunkedUploadApiViewSet,
+    basename="chunk"
+)
+
---
+++ chunkedupload/uploads/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path
+from uploads.routers import CommonRouter
+from django.urls import include
+
+urlpatterns = [
+    path('api/', include(CommonRouter.urls)),
+]
